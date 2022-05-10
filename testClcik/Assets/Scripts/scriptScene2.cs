86013ffb915728d3379783d11d3665fe45cf73db
lam1to <xtxtx96@mail.ru>
2022-05-10 18:44:08 +0300

    smenScen
    Add smen scen

---
+++ testClcik/Assets/Scripts/scriptScene2.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class scriptScene2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void smenscen()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void smenscen2()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
+}
