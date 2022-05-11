17f3065ae1936a219d75e30c808eebbe6bbbb816
lam1to <xtxtx96@mail.ru>
2022-05-11 22:13:25 +0300

    Crate function in shop

--- testClcik/Assets/Scripts/Shop.cs
+++ testClcik/Assets/Scripts/Shop.cs
@@ -49,9 +49,49 @@ public class Shop: MonoBehaviour
         }
     }
     */
+    int switchCost = 100;
+    int LubricationCost = 10;
+
+    public TextMeshProUGUI SwitchCostText;
+    public TextMeshProUGUI LubricationCostText;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("first") == 0)
+        {
+            PlayerPrefs.SetInt("first", 1);
+            PlayerPrefs.SetInt("switchCost", switchCost);
+            PlayerPrefs.SetInt("LubricationCost", LubricationCost);
+        }
+
+        SwitchCostText.text = PlayerPrefs.GetInt("switchCost") + " click";
+        LubricationCostText.text = PlayerPrefs.GetInt("LubricationCost") + " click";
+    }
 
 
-    public Text KeyboardCost;
+    public void Switch()
+    {
+        if (PlayerPrefs.GetInt("scor") >= PlayerPrefs.GetInt("switchCost"))
+        {
+            PlayerPrefs.SetInt("scor", PlayerPrefs.GetInt("scor") - PlayerPrefs.GetInt("switchCost"));
+            PlayerPrefs.SetInt("switchCost", (int)(PlayerPrefs.GetInt("switchCost") * 1.5f));
+            PlayerPrefs.SetInt("clickup", PlayerPrefs.GetInt("clickup") * 3);
+            SwitchCostText.text = PlayerPrefs.GetInt("switchCost") + " click";
+        }
+
+    }
+    public void Lubrication()
+    {
+        if (PlayerPrefs.GetInt("scor") >= PlayerPrefs.GetInt("LubricationCost"))
+        {
+            PlayerPrefs.SetInt("scor", PlayerPrefs.GetInt("scor") - PlayerPrefs.GetInt("LubricationCost"));
+            PlayerPrefs.SetInt("LubricationCost", (int)(PlayerPrefs.GetInt("LubricationCost") * 1.5f));
+            PlayerPrefs.SetInt("clickup", PlayerPrefs.GetInt("clickup") + 1);
+            LubricationCostText.text = PlayerPrefs.GetInt("LubricationCost") + " click";
+        }
+    }
+
+
 
 
 
